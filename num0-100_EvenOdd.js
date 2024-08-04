a8377b9edec076ad6398f063f2e31e1838317043
ruchi-codes <ruchi06052005@gmail.com>
2024-08-05 00:31:36 +0530

    Even and Odd Numbers 0-100

---
+++ num0-100_EvenOdd.js
@@ -0,0 +1,24 @@
+//Print 0 to 100
+for(let num = 1; num<=100; num++)
+{
+    console.log("Num",num)
+}
+
+//Print Even number from 0 to 100
+for(let i = 1; i<=100; i++)
+    {
+        if(i % 2 === 0)
+        {
+            console.log("Even number is",i);
+        }
+        
+    }
+
+//Print Odd number from 0 to 100
+for(let i = 1; i<=100; i++)
+    {
+        if(i % 2 !== 0)
+        {
+            console.log("Odd number is",i);
+        }
+    }    
