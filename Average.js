53de216e0d63c434ce38bc220e30e96381c36f08
ruchi-codes <ruchi06052005@gmail.com>
2024-08-08 22:29:20 +0530

    Sum of n

--- Average.js
+++
@@ -1,9 +0,0 @@
-let marks = [85,97,44,37,76,60]
-let sum = 0;
-for(let val of marks)
-{
-    sum += val;   //Sum = sum + val
-}
-
-let average = sum / marks.length;
-console.log(`Average of the class is ${average}`);
---
+++ CalculateSum.js
@@ -0,0 +1,8 @@
+//Calculate sum of 1 to 5
+
+let sum = 0;
+for(let i = 0; i <= 5; i++)
+{
+    sum = sum + i
+}
+console.log(sum);
